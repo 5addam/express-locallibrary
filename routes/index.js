56969ff5d913c0460182c8c8bbc0e67cea0f3be6
5addam <saddam.mughal@protonmail.com>
2021-06-08 17:42:20 +0500

    mongoose implemented

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/admin', function(req, res, next) {
+  res.send("You're not authorized!!");
+});
+
 module.exports = router;
